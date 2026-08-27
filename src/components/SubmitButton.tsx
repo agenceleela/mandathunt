04355db258d63a7b3f5b996d8d80b90e8070d248
agenceleela <agenceleela@gmail.com>
2026-08-27 18:55:08 +0200

    fix: SubmitButton import useFormStatus from react-dom

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useFormStatus } from "react";
+import { useFormStatus } from "react-dom";
 
 interface SubmitButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
